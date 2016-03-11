049d2d439594cf437421bd5fd96fe31c14d08d4a
David Hatch <dhatch@rayzist.com>
2016-03-11 09:43:22 -0800

    Adds new selenium test for searching bing. Adds shuffled tech wordlist to use and shell script to update the selenium test with a new word.

---
+++ update_selenium_search.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#set -x
+
+wordListFile="$HOME/bin/tech_wordlist.txt"
+scriptFile="$HOME/bin/selenium_test_30.html"
+
+# Get index from 1st line of file and then increment
+read -r index < "$wordListFile"
+(( index++ ))
+
+# Loop the word list when the last word is used
+lineCount=$(wc -l "$wordListFile" | awk '{print $1}')
+if [[ $index -gt $lineCount ]];then
+    index=2
+fi
+
+# Get the new word from that line index
+newWord="$(sed -n ''$index' p' <"$wordListFile")"
+
+# Replace script with new search word and update index in wordlist
+sed -i.bak "27 s/>.*</>$newWord</" "$scriptFile" && sed -i.bak '1 s/^.*$/'$index'/' "$wordListFile"
