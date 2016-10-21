1abc7a89d88fb5c324d1849af065affc01b9fdd8
David Hatch <dhatch@rayzist.com>
2016-10-21 08:01:44 -0700

    Serial Lookup - Adds script to search the company software serials reference.  Works on mac and cygwin. Requires mount_net_drive.sh script to connect to server.

---
+++ serial_lookup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#set -x
+
+# Mac OSX file paths
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    file='/Volumes/I - Developement/My Docs/Reference/Our Serial Numbers.txt'
+    if [[ ! -e $file ]]; then
+        # If file doesn't exist then try to mount the share
+        if [[ -e mount_net_drive.sh ]]; then
+           mount_net_drive.sh i
+        else
+           echo -e "Error: Can't find file: $file \nPlease mount 'vm2/I - Developement' then try again"
+           exit 2
+       fi
+    fi
+
+# Cygwin file paths
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    file='/cygdrive/i/My Docs/Reference/Our Serial Numbers.txt'
+fi
+
+if [[ -n $* ]]; then
+    grep -Ei --color=auto "$*" "$file"
+else
+    echo "Error: Requires at least 1 param used to search with"
+    exit 1
+fi
