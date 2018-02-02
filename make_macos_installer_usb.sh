381189797da46aa35c39233f0f2da349518711dc
David Hatch <dhatch@rayzist.com>
2018-02-02 15:19:38 -0800

    make_macos_installer_usb - Updates script to more robust with a simple menu for selecting the destination.

--- make_macos_installer_usb.sh
+++ make_macos_installer_usb.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
-installer=$(ls -dt /Applications/Install*.app/ | head -1)
+### Find newest installer
+installer=$(ls -dt /Applications/Install\ *.app/ | head -1)
 
 sourceLocation="$installer"Contents/Resources/createinstallmedia
-destination=$(ls -d /Volumes/Install\ macOS*/)
+destination=''
 
+### Find the destination
+x=1
+IFS='
+'
+for line in $(df -h | grep disk); do
+    devices[$x]=$line
+    echo "$x. $line"
+    (( x++ ))
+done
+unset IFS
 
-read -n 1 -p "Found install file: $installer
-Found destination:
-$(df -h |awk 'NR==1 {print}; /Volumes\/Install mac/ {print}')
+read -p "Select destination volume...Enter number [1-$((x-1))]: " number
+selection="${devices[$number]}"
+destination="${selection/#*Volumes//Volumes}"
+echo
+
+read -n 1 -p "Install file: $installer
+Destination: $destination
 
 Please confirm to continue (Y/N) " answer
 echo
 
 if [[ $answer == [yY] ]]; then
-    sudo "$sourceLocation" --volume "$destination" --nointeraction && say Boot Installer Complete
+    if [[ -d $destination ]]; then
+        sudo "$sourceLocation" --volume "$destination" --nointeraction && say Boot Installer Complete
+    else
+        echo "Destination is not valid. Try again"
+        exit 1
+    fi
 fi
 
-exit
+exit 0
