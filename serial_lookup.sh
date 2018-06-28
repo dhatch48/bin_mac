d4a9bc7b83fd82c0cd526e7d1abd1ad16d2ded31
David Hatch <dhatch@rayzist.com>
2018-06-28 11:02:35 -0700

    serial_lookup - Fixes mount_net_drive not working when current location is not ~/bin.

--- serial_lookup.sh
+++ serial_lookup.sh
@@ -6,10 +6,10 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     file='/Volumes/I - Developement/My Docs/Reference/Our Serial Numbers.txt'
     if [[ ! -e $file ]]; then
         # If file doesn't exist then try to mount the share
-        if [[ -e mount_net_drive.sh ]]; then
+        if $(type mount_net_drive.sh &>/dev/null); then
            mount_net_drive.sh i
         else
-           echo -e "Error: Can't find file: $file \nPlease mount 'vm2/I - Developement' then try again"
+           echo -e "Error: Can't find file: $file \nPlease mount '//vm2/I - Developement' then try again"
            exit 2
        fi
     fi
