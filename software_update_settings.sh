20f2247e1225d820aea7211fc962c3500aafa5a6
David Hatch <dhatch@rayzist.com>
2018-11-26 16:39:58 -0800

    software_update_settings - Changes AutomaticDownload to NO. This is to prevent Mac OS upgrades from automatically downloading.

--- software_update_settings.sh
+++ software_update_settings.sh
@@ -1,9 +1,8 @@
-# TOGGLE ALL ON except for Mac OS updates
 # before setting values quit system preferences & stop software update - stops deafults cache breaking 'AutomaticCheckEnabled'
 osascript -e "tell application \"System Preferences\" to quit"
 sudo softwareupdate --schedule off
 sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist AutomaticCheckEnabled -bool YES
-sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist AutomaticDownload -bool YES
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist AutomaticDownload -bool NO
 sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist ConfigDataInstall -bool YES
 sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist CriticalUpdateInstall -bool YES
 sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist ScheduleFrequency -int 5
